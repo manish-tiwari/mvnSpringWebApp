ff68f4fbbf203ca7168f622389db2c6f09c06fa9
Manish Tiwari <mtiwari5@gmail.com>
2019-02-12 18:06:08 +0530

    Initial Commit: Create POJOs

---
+++ src/main/java/manish/springframework/mvnSpringWebApp/MvnSpringWebAppApplication.java
@@ -0,0 +1,14 @@
+package manish.springframework.mvnSpringWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnSpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnSpringWebAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/manish/springframework/mvnSpringWebApp/MvnSpringWebAppApplicationTests.java
@@ -0,0 +1,17 @@
+package manish.springframework.mvnSpringWebApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvnSpringWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
