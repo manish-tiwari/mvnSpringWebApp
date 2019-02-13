b2ee44ed0f175f89df8dd751385c5c5f2b8479d4
Manish Tiwari <mtiwari5@gmail.com>
2019-02-13 11:50:06 +0530

    Equality in Hibernate

--- src/main/java/manish/springframework/mvnSpringWebApp/model/Author.java
+++ src/main/java/manish/springframework/mvnSpringWebApp/model/Author.java
@@ -2,6 +2,7 @@ package manish.springframework.mvnSpringWebApp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -57,8 +58,33 @@ public class Author {
         this.lastName = lastName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
     public Long getId() {
         return id;
+
     }
 
     public void setId(Long id) {
--- src/main/java/manish/springframework/mvnSpringWebApp/model/Book.java
+++ src/main/java/manish/springframework/mvnSpringWebApp/model/Book.java
@@ -2,6 +2,7 @@ package manish.springframework.mvnSpringWebApp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -36,7 +37,33 @@ public class Book {
         this.authors = authors;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
     public String getTitle() {
+
         return title;
     }
 
