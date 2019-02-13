2e9b9667bae27762b86a0904aca94a44e91d8af8
Manish Tiwari <mtiwari5@gmail.com>
2019-02-13 11:42:51 +0530

    Use Spring Data JPA Repo

---
+++ src/main/java/manish/springframework/mvnSpringWebApp/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package manish.springframework.mvnSpringWebApp.repositories;
+
+import manish.springframework.mvnSpringWebApp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Manish Tiwari on 2/13/2019.
+ */
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
---
+++ src/main/java/manish/springframework/mvnSpringWebApp/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package manish.springframework.mvnSpringWebApp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Manish Tiwari on 2/13/2019.
+ */
+public interface BookRepository extends CrudRepository {
+}
